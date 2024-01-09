62bc833d20a4dc51c25980debe8aa58611c58fbe
13180 <sean.allen-poole@vtsd.com>
2024-01-09 13:12:03 -0500

    Stars and loops

---
+++ Challenges/challenge4/formCheck.js
@@ -0,0 +1,22 @@
+// Gain Access to form elements
+const submitButton = document.getElementById('submit');
+
+// Add event listener to submit button
+submitButton.addEventListener('click', checkForm);
+
+
+function checkForm(){
+    /*
+        In this method check whether name, email, and phone number have input.
+        If they are blank turn the fields red.  Other-wise do nothing.
+     */
+
+    const name = HTMLElement = document.getElementById('name')
+
+    if(
+        name.value === ''){
+
+    }
+
+
+}
